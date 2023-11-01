e4f12a0c4fc7bf142405396a149a939224b34007
cleiton97 <146895519+cleiton97@users.noreply.github.com>
2023-11-01 00:16:32 +0000

    Arrumado erro de apostrofo no I'm

--- app/componetes/Banner.tsx
+++ app/componetes/Banner.tsx
@@ -9,7 +9,7 @@ export default function Banner() {
             
         <div className="basis-1/2"></div>
         <div className="basis-1/2 text-center  text-white">                
-            <h3 className="text-2xl">Hi, I'm a student, another fine</h3>
+            <h3 className="text-2xl">Hi, I`&apos`m a student, another fine</h3>
             <h4 className="text-2xl">little freebie from UTFPR.</h4>
             <h4>Accumsan feugiat mi commodo erat lorem ipsum, sed magna</h4>
             <h4>lobortis feugiat sapien sed etiam volutpat accumsan.</h4>
